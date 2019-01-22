673f478f8fbbc680f9f2c5c9bb78cc9ccb1cafaa
Ron Valente <ronald.valente@gmail.com>
2019-01-22 07:01:55 -0500

    added initial openbsd openstack test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # openbsd-openstack-image
+
 Packer Build of OpenBDS 6.4 for use in OpenStack
+
+The objective here is to have a very small OpenBSD image for use with OpenStack.
+
+There are some functionality that wont work, like auto-resize of the root disk.
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,13 @@
+set -x
+
+# ensure we have curl for rc.firsttime
+pkg_add curl
+
+# Allow openbsd user to login as root
+echo 'permit openbsd as root' > /etc/doas.conf
+
+# disable root ssh
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+
+# remove ssh host key
+rm -rfv /etc/ssh/*key*
