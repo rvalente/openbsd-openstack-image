2c0a8d41f95a4d37949cc6c08b531e3f5113a8b2
Ron Valente <ronald.valente@gmail.com>
2019-01-24 05:06:54 -0500

    ensure we have dhcp setup for vio0 interface

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,3 +13,9 @@ sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 
 # remove ssh host key
 rm -rfv /etc/ssh/*key*
+
+# remove hostname.em0
+rm /etc/hostname.em0
+
+# setup dhcp for hostname.vio0
+echo 'dhcp' > /etc/hostname.vio0
